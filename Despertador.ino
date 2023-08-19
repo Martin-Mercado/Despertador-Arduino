9a65cb339837950eef75b961fee00d5c9345b69d
Martin Mercado <68834126+Martin-Mercado@users.noreply.github.com>
2023-08-19 19:19:11 -0300

    Update Despertador.ino

--- Despertador.ino
+++ Despertador.ino
@@ -1,46 +1,48 @@
 #include <virtuabotixRTC.h>
 #include <LiquidCrystal_I2C.h>
+
+// Pin Definitions
 #define pinBuzzer 5
 #define pinMainButton 4
 #define pinLeftButton 12
 #define pinRightButton 13
 
+// Initialize LCD display with I2C address 0x27, 18 columns, and 2 rows
 LiquidCrystal_I2C lcd(0x27, 18, 2);
+
+// Initialize RTC with pins 6, 7, and 8 for DS1302 module
 virtuabotixRTC myRTC(6, 7, 8);
 
-
+// Global Variables
 bool alarmSet = true;
 int alarmHour = 18;
 int alarmMin = 0;
 
 void setup() {
-
+  // Configure button pins as inputs
   pinMode(pinMainButton, INPUT);
   pinMode(pinLeftButton, INPUT);
   pinMode(pinRightButton, INPUT);
-  // segundos, minutos, horas, día de la semana, día del mes, mes, año
 
-  //myRTC.setDS1302Time(0, 40, 19, 6,17,2,2023);
-
+  // Initialize LCD and turn on backlight
   lcd.init();
   lcd.clear();
   lcd.backlight();
 }
 
 void loop() {
-
+  // Store current minute for comparison
   int NowMinute = myRTC.minutes;
 
+  // Update RTC time
   myRTC.updateTime();
 
-
-  //resetear lcd asi no queda el digito que sobra del ultimo minuto
+  // Reset LCD display when a new minute starts
   if (myRTC.minutes == NowMinute + 1) {
-
     lcd.clear();
   }
 
-  //boton para poner y apagar la alarma
+  // Toggle alarm set status when main button is pressed
   if (digitalRead(pinMainButton) && alarmSet == false) {
     alarmSet = true;
     lcd.clear();
@@ -58,9 +60,7 @@ void loop() {
     lcd.clear();
   }
 
-
-  //Fecha
-
+  // Display current date
   lcd.setCursor(0, 0);
   lcd.print(myRTC.dayofmonth);
   lcd.print("/");
@@ -68,16 +68,15 @@ void loop() {
   lcd.print("/");
   lcd.print(myRTC.year);
 
-  //hora
+  // Display current time
   lcd.setCursor(1, 1);
-
   lcd.print(myRTC.hours);
   lcd.print(":");
   lcd.print(myRTC.minutes);
   lcd.print(":");
   lcd.print(myRTC.seconds);
 
-  //mostrar hora de alarma si esta seteada, sinon mostrar que no esta seteada
+  // Display alarm time if set, otherwise show "NOT SET"
   if (alarmSet) {
     lcd.setCursor(11, 0);
     lcd.print("SET");
@@ -85,7 +84,6 @@ void loop() {
     lcd.print(alarmHour);
     lcd.print(":");
     lcd.print(alarmMin);
-
   } else {
     lcd.setCursor(11, 0);
     lcd.print("NOT");
@@ -93,28 +91,28 @@ void loop() {
     lcd.print("SET");
   }
 
-  //cambiar la hora de la alaema con boton izquierdo
+  // Change alarm time using left button
   if (digitalRead(pinLeftButton)) {
     SetAlarm();
   }
 
-  // apagar pantalla porque molesta
+  // Turn off display temporarily using right button
   if (digitalRead(pinRightButton)) {
     lcd.noBacklight();
     delay(1000);
 
-    //calculate hour to turn on display
+    // Calculate hour to turn on display
     int HourTurnedOff = myRTC.hours, HourToTurnOn;
-
     if (HourTurnedOff >= 0 && HourTurnedOff <= 15) {
       HourToTurnOn = HourTurnedOff + 8;
     } else if (HourTurnedOff > 15 && HourTurnedOff < 23) {
       HourToTurnOn = HourTurnedOff - 16;
     }
+
+    // Wait until HourToTurnOn matches current hour or right button is pressed
     while (true) {
       myRTC.updateTime();
       delay(200);
-
       if (digitalRead(pinRightButton) || HourToTurnOn == myRTC.hours) {
         lcd.backlight();
         break;
@@ -122,15 +120,14 @@ void loop() {
     }
   }
 
-
-
+  // Check if alarm should go off
   CheckAlarm();
 
-
+  // Add delay for stability
   delay(200);
 }
 
-// Hacer ruidito de alarma
+// Generate a buzz sound using the buzzer
 void buzz() {
   tone(pinBuzzer, 1000);
   delay(100);
@@ -138,9 +135,8 @@ void buzz() {
   delay(150);
 }
 
-
+// Display the alarm time on the LCD
 void printAlarmTime() {
-
   lcd.setCursor(5, 1);
   lcd.print(alarmHour);
   lcd.print(":");
@@ -148,12 +144,9 @@ void printAlarmTime() {
   lcd.print("    ");
 }
 
-
-//check if alarm should go off
+// Check if alarm should go off
 void CheckAlarm() {
-  // prender alarma
   if (alarmHour == myRTC.hours && alarmMin == myRTC.minutes && alarmSet && myRTC.seconds == 0) {
-    //fecha
     lcd.clear();
     lcd.setCursor(3, 0);
     lcd.print(myRTC.dayofmonth);
@@ -162,15 +155,14 @@ void CheckAlarm() {
     lcd.print("/");
     lcd.print(myRTC.year);
 
-    //hora
     lcd.setCursor(4, 1);
-
     lcd.print(myRTC.hours);
     lcd.print(":");
     lcd.print(myRTC.minutes);
     lcd.print(":");
     lcd.print(myRTC.seconds);
 
+    // Sound the alarm for a specified duration
     int secondAlarmCalled = myRTC.seconds;
     for (int i = 1200; i >= 0; i--) {
       if (digitalRead(pinMainButton)) {
@@ -178,24 +170,21 @@ void CheckAlarm() {
         lcd.clear();
         break;
       }
-
       buzz();
     }
   }
 }
 
-
-//funcion para setear a que hora suena la alarma
+// Set the alarm time using buttons
 void SetAlarm() {
-  //set Hours
   lcd.clear();
   bool HoursSet = true;
+
+  // Set the alarm hour
   while (HoursSet) {
     delay(40);
-
     if (digitalRead(pinLeftButton) && alarmHour != 0) {
       alarmHour -= 1;
-
     } else if (digitalRead(pinRightButton) && alarmHour < 23) {
       alarmHour += 1;
     } else if (digitalRead(pinMainButton)) {
@@ -206,14 +195,12 @@ void SetAlarm() {
     printAlarmTime();
   }
 
-
-  //set Minutes
+  // Set the alarm minutes
   bool MinutesSet = true;
   lcd.setCursor(0, 0);
   lcd.print("Set Alarm Minute: ");
   printAlarmTime();
   delay(2600);
-
 
   while (MinutesSet) {
     delay(40);
@@ -229,6 +216,7 @@ void SetAlarm() {
     printAlarmTime();
   }
 
+  // Display confirmation and set alarm time
   lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("Alarm Set: ");
